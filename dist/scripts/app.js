f682d3573eb1d309982f58faa9e8e6a3f5a1761d
tuckermullen <mullentucker@gmail.com>
2016-10-25 10:37:47 -0700

    In progress 1

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -13,8 +13,26 @@
                 templateUrl: '/templates/home.html'
         });
       }
+    
+    (function() {
+        function BlocChatCookies($cookies, $uibModal) {
+            var currentUser = $cookies.get('blocChatCurrentUser');
+            if (!currentUser || currentUser === '') {
+                $uibModal.open({
+                    templateUrl: 'usernameModal.html',
+                    controller: function($uibModalInstance) {
+                        this.newUsername = '';
+                        this.create = function(username) {
+                            $uibModalInstance.close(this.newUsername);
+                        };
+                    }
+                });
+            }
+        }
+    });
 
     angular
-        .module('blocChat', ['ui.router', 'firebase', 'ui.bootstrap'])
+        .module('blocChat', ['ui.router', 'firebase', 'ui.bootstrap', 'ngCookies'])
         .config(config)
+        .run(['$cookies', '$uibModal', BlocChatCookies]);
  })();
